833a5a9690b6736641da6cc8300688643a534693
Angga7Togk <kiplihode123321@gmail.com>
2025-07-29 20:53:58 +0700

    fix

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,7 +1,6 @@
 import { defineMiddleware } from '@gaman/core/middleware';
 
 export default defineMiddleware((ctx) => {
-	const token = ctx.header('Authorization').split('Bearer ')[1];
 	// todo your middleware
 	return next();
 });
